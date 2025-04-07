33f48c40fbaf8c5a60fff08c08ab09dbb97922e3
shuailiu1990 <shuai.liu.1990@foxmail.com>
2025-04-07 09:05:24 +0800

    update

--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -1,13 +1,14 @@
 return {
-	"iamcco/markdown-preview.nvim",
-	cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-	ft = { "markdown" },
-	keys = {
-		{ "<leader>cp", "<cmd>MarkdownPreview<CR>", desc = "MarkdownPreview" },
-		{ "<leader>cx", "<cmd>MarkdownPreviewStop<CR>", desc = "Markdown Preview Stop" },
-		--{ "<leader>mP", "<cmd>MarkdownPreviewToggle<CR>", desc = "MarkdownPreviewToggle" },
-	},
-	build = function()
-		vim.fn["mkdp#util#install"]()
-	end,
+  "iamcco/markdown-preview.nvim",
+  event = "VeryLazy",
+  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+  ft = { "markdown" },
+  keys = {
+    { "<leader>cp", "<cmd>MarkdownPreview<CR>", desc = "MarkdownPreview" },
+    { "<leader>cx", "<cmd>MarkdownPreviewStop<CR>", desc = "Markdown Preview Stop" },
+    --{ "<leader>mP", "<cmd>MarkdownPreviewToggle<CR>", desc = "MarkdownPreviewToggle" },
+  },
+  build = function()
+    vim.fn["mkdp#util#install"]()
+  end,
 }
