0f10b814d09f6615c1c2aaaa4984aeabc83f360a
shuailiu1990 <shuai.liu.1990@foxmail.com>
2025-08-16 17:03:31 +0800

    update

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -6,7 +6,7 @@ vim.opt.shiftwidth = 4 -- number of spaces to use for each step of (auto)indent
 vim.opt.softtabstop = 4 -- number of spaces that a <Tab> counts for while performing editing operations
 vim.opt.expandtab = true -- use spaces instead of tabs
 vim.opt.relativenumber = false -- disable relativenumber
--- vim.opt.wrap = true
+vim.opt.wrap = true
 --
 -- function _G.set_terminal_keymaps()
 --   local opts = { buffer = 0 }
