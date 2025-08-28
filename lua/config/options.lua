51d3e63d6f05c5de1c5d14b741b55e04b35dfe11
shuailiu1990 <shuai.liu.1990@foxmail.com>
2025-08-28 09:05:57 +0800

    update

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,13 +1,14 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
 vim.opt.tabstop = 4 -- number of spaces that a <Tab> in the file counts for
 vim.opt.shiftwidth = 4 -- number of spaces to use for each step of (auto)indent
 vim.opt.softtabstop = 4 -- number of spaces that a <Tab> counts for while performing editing operations
 vim.opt.expandtab = true -- use spaces instead of tabs
 vim.opt.relativenumber = false -- disable relativenumber
 vim.opt.wrap = true
---
+-- Terminal in neovim
 -- function _G.set_terminal_keymaps()
 --   local opts = { buffer = 0 }
 --   vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
@@ -18,6 +19,6 @@ vim.opt.wrap = true
 --   vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts)
 --   vim.keymap.set("t", "<C-w>", [[<C-\><C-n><C-w>]], opts)
 -- end
---
--- -- if you only want these mappings for toggle term use term://*toggleterm#* instead
+
+-- if you only want these mappings for toggle term use term://*toggleterm#* instead
 -- vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
